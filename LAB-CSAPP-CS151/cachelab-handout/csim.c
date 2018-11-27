51e1bb51f7026ba09186cf6f54f2204a59f58570
Ubuntu <huqianshan_uestc@163.com>
2018-11-27 22:32:48 +0800

    add some thing about cachelab 1.0/10

--- LAB-CSAPP-CS151/cachelab-handout/csim.c
+++ LAB-CSAPP-CS151/cachelab-handout/csim.c
@@ -2,7 +2,7 @@
 #include<unistd.h> // for getopt
 #include<stdlib.h> // for atoi
 #include<string.h>
-#include<stdio.h>
+#include<stdio.h> //fopen 
 
 typedef struct{
     int valid;
@@ -35,10 +35,9 @@ int get_opt(int argc,char **argv,int *s,int* E,int *b,char* trace_filename,int* 
             case 't': checkOptarg(optarg);
             strcpy(trace_filename,optarg);
             break;
-            case 'h': printHelpMenu();
-            exit(0);
-            //default:
-                //printHelpMenu();
+            case 'h': 
+            default:
+                printHelpMenu();
                 exit(0);
         }
     }
@@ -67,16 +66,64 @@ void printHelpMenu(){
     printf("linux>  ./csim -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
 }
 
+int initSimCache(int s,int e,int b,Sim_Cache* cache){
+    if(s<0){
+        printf("Set number error \n");
+        exit(-1);
+    }
+    
+    cache->set_num=2<<s;
+    cache->line_num=e;
+    cache->sets=(Set*)malloc(cache->set_num*sizeof(Set));
+
+    if(!cache->set_num){
+        printf("Set Memory error \n");
+        exit(-2);
+    }
+
+    int i,j;
+    for(i=0;i<cache->set_num;i++){
+        cache->sets[i].lines=(Line*)malloc(e*sizeof(Line));
+
+        if(!cache->set_num){
+        printf("Set Memory error \n");
+        exit(-2);
+        }
+
+        for(j=0;j<e;j++){
+            cache->sets[i].lines[j].valid=0;
+            cache->sets[i].lines[j].lru_num=0;
+        }
+    }
+
+    return 0;
+}
+
 int main(int argc,char** argv)
 {
     int s,E,b,is_verbose=0;
-    char trace_file[100],opt[10];
+    char trace_name[100],opt[10];
 
     Sim_Cache cache;
 
     // 1. parse get_opt(argc,argv,)
-    int tem=get_opt(argc,argv,&s,&E,&b,trace_file,&is_verbose);
-    printf("\n tem:%d",tem);
-    printf("s:%d E:%d b:%d filename:%s verbose:%d",s,E,b,trace_file,is_verbose);
+    int tem=get_opt(argc,argv,&s,&E,&b,trace_name,&is_verbose);
+    //printf("\n tem:%d \n",tem);
+    //printf("s:%d E:%d b:%d filename:%s verbose:%d \n",s,E,b,trace_file,is_verbose);
+
+    // 2. initilize cache struct
+    int cache_tem=initSimCache(s,E,b,cache);
+    
+    // 3. open the trace file
+    FILE* trace_file=fopen(trace_name,"r");
+
+    // 4. read data from trace fle
+    int addr,size;
+    while(fscanf(trace_file,"%s %x %d",opt,&addr,&size)!=EOF){
+        // 4.1 Instruction
+        if(strcmp(opt,"I")==0) continue;
+
+        int
+    }
     return 0;
 }
