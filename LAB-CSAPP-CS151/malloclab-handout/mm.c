fae34dd4f7b9792593b42c5fa9cdaa780f4b385b
Ubuntu <huqianshan_uestc@163.com>
2018-12-12 17:16:14 +0800

    fininshed mm_int extend_heap coalesce Macro

--- LAB-CSAPP-CS151/malloclab-handout/mm.c
+++ LAB-CSAPP-CS151/malloclab-handout/mm.c
@@ -45,11 +45,101 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+/*basic constants and macros of mine*/
+#define  WSIZE 4
+#define  DSIZE 8
+#define CHUNKSIZE (1<<12)
+
+#define  GET(p)        (*(unsigned int*)(p))
+#define  PUT(p,val)    (*(unsigned int*)(p) = (val))
+
+/* size plus alloc*/
+#define  PACK(size,alloc) ((size)|(alloc))
+
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+/* bp is block pointer it pointers to the first valid byte*/
+#define HDRP(bp) ((char*)(bp)-WSIZE)
+#define FTRP(bp) ((char*)(bp)+GET_SIZE(HDRP(bp))-DSIZE)
+
+#define NEXT_BLKP(bp) ((char*)(bp)+GET_SIZE((HDRP(bp))))
+#define PREV_BLKP(bp) ((char*)(bp)-GET_SIZE(((char*)(bp)-DSIZE)))
+
+static char *heap_listp=0;
+
+static void* coalesce(void* bp){
+    size_t bef_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t aft_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size=GET_SIZE(HDRP(bp));
+
+    /* case One both before and after block is malloced*/
+    if(bef_alloc==1 && aft_alloc==1){
+        return bp;
+    }
+    /* case Two Only before is malloced*/
+    if(bef_alloc==1 && aft_alloc==0){
+        size+=GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp),PACK(size,0));
+        /*Here Attention!!! PUT has already changed the size
+        * and the FTRP is calcaulate the footer blook 
+        * by the size */
+       PUT(FTRP(bp),PACK(size,0));
+    }
+    /* case Three Only after block is malloced*/
+    if(bef_alloc==0 && aft_alloc==1){
+        size+=GET_SIZE(FTRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp),PACK(size,0));
+        PUT(HDRP(PREV_BLKP(bp)),PACK(size,0));
+        bp=PREV_BLKP(bp);
+    }
+    /* case Four both before and after block is Not malloced*/
+    if(bef_alloc==0 && aft_alloc ==0){
+        size+=GET_SIZE(HDRP(NEXT_BLKP(bp)))+GET_SIZE(FTRP(PREV_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)),PACK(size,0));
+        PUT(FTRP(NEXT_BLKP(bp)),PACK(size,0));
+        bp=PREV_BLKP(bp);
+    }
+
+    return bp;
+}
+
+static void *extend_heap(size_t words){
+    char *bp;
+    size_t size;
+
+    size = (words%2)?(words+1)*WSIZE:(words)*WSIZE;
+    if((long)(bp=mem_sbrk(size))==-1){
+        return NULL;
+    }
+
+    PUT(HDRP(bp),PACK(size,0));
+    PUT(FTRP(bp),PACK(size,0));
+    PUT(HDRP(NEXT_BLKP(bp)),PACK(0,1));
+
+    return coalesce(bp);
+}
+
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {   
+    if((heap_listp=mem_sbrk(4*WSIZE))==(void*)-1){
+        return -1;
+    } 
+
+    PUT(heap_listp,0);
+    PUT(heap_listp+(1*WSIZE),PACK(DSIZE,1));
+    PUT(heap_listp+(2*WSIZE),PACK(DSIZE,1));
+    PUT(heap_listp+(3*WSIZE),PACK(0,1));
+
+    heap_listp+=(2*WSIZE);
+
+    if(extend_heap(CHUNKSIZE/WSIZE)==NULL){
+        return -1;
+    }
     return 0;
 }
 
