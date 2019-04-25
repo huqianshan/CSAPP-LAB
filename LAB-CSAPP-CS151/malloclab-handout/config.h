d0016d69f888cdf3a45093ac16ec1465af26463e
Ubuntu <huqianshan_uestc@163.com>
2019-04-25 16:06:25 +0800

    some config

--- LAB-CSAPP-CS151/malloclab-handout/config.h
+++ LAB-CSAPP-CS151/malloclab-handout/config.h
@@ -32,9 +32,9 @@
   "binary-bal.rep",\
   "binary2-bal.rep",\
   "realloc-bal.rep",\
-  "short1-bal.rep",\
-  "short2-bal.rep",\
   "realloc2-bal.rep"
+  // "short1-bal.rep",
+  // "short2-bal.rep",
 /*
  * This constant gives the estimated performance of the libc malloc
  * package using our traces on some reference system, typically the
